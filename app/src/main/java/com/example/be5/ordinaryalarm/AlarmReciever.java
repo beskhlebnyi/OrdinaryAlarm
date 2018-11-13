46554e34d5faf1cf04e7c36c7d2e01b740569a1c
be5 <beskhlebnyi@gmail.com>
2018-11-13 09:38:07 +0200

    add AlarmReciever activity

---
+++ app/src/main/java/com/example/be5/ordinaryalarm/AlarmReciever.java
@@ -0,0 +1,13 @@
+package com.example.be5.ordinaryalarm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmReciever extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_reciever);
+    }
+}
